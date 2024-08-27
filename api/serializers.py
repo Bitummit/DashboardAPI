d9a5494945a45bd462ad8490f1c4f5fc756d2468
Papacita <ignat.700@mail.ru>
2024-08-27 09:59:16 +0300

    fix jwt

--- api/serializers.py
+++ api/serializers.py
@@ -136,7 +136,6 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
         token['username'] = user.username
-        token['email'] = user.email
 
         return token
 
--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from . import views
 from rest_framework_simplejwt.views import (
-    TokenRefreshView,
+    TokenRefreshView,TokenObtainPairView
 )
 
 
 urlpatterns = [
     path('users/', views.UserListView.as_view(), name="users"),
-    path('jwt/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('jwt/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.RegisterView.as_view(), name='register'),
 
